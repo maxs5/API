31a5b458f0decde8be59eb5d17a2167977496128
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-02-10 21:26:24 +0300

    Initial commit

---
+++ src/main/java/ru/netology/api/FormDate.java
@@ -0,0 +1,8 @@
+package ru.netology.api;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+}
---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.api;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+}
